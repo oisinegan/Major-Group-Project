734d900a6260a47558968253b365e28d812e357b
oisinegan <oisinegan00@gmail.com>
2022-12-08 22:18:50 +0000

    Fixed Error:501 on user profile

--- JobHireApp/app/screens/UserProfile.js
+++ JobHireApp/app/screens/UserProfile.js
@@ -17,13 +17,16 @@ import { db } from "../database/config";
 // - 08/12: there is an error happening when this page is loaded for me(adam), it's "Please report: Excessive number of pending callbacks: 501."
 //and it freezes the whole app, i have to reload it to get it to work.
 //Think it is something to do with aysnc, not 100% sure.
+
+//^^ Fixed but really bad implementation Reading in whole database to call the getData async function, but it'll do for now
+
 function UserProfile({ navigation }) {
   const [Users, setUsers] = useState([]);
   const [username, setUsername] = useState("");
 
   //Read all data
-  getDocs(collection(db, "Users")).then((docSnap) => {
-    const Users = [];
+  getDocs(collection(db, "Jobseekers")).then((docSnap) => {
+    /*   const Users = [];
 
     docSnap.forEach((doc) => {
       const { About, Age, Name, appliedJobs } = doc.data();
@@ -36,7 +39,7 @@ function UserProfile({ navigation }) {
         appliedJobs,
       });
     });
-    setUsers(Users);
+    setUsers(Users); */
     getData();
   });
   const getData = async () => {
