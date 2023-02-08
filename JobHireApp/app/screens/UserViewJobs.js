e35f2126735baa7ace161b845887f1062d914c48
Adamf44 <b00134787@mytudublin.ie>
2023-02-08 14:25:28 +0000

    UserViewJobs update

--- JobHireApp/app/screens/UserViewJobs.js
+++ JobHireApp/app/screens/UserViewJobs.js
@@ -3,38 +3,220 @@ import {
   Text,
   View,
   Button,
-  Alert,
-  ScrollView,
-  Image,
-  TextInput,
-  Pressable,
+  SafeAreaView,
+  FlatList,
   TouchableOpacity,
-  KeyboardAvoidingView,
-  PermissionsAndroid,
+  Image,
 } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as React from "react";
+import { useState } from "react/cjs/react.development";
+import { useEffect } from "react/cjs/react.development";
+import {
+  collection,
+  doc,
+  setDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../database/config";
 
 function UserViewJobs({ navigation }) {
+  
+  const [username, setUsername] = useState("");
+  const [userJobs, setUserJobs] = useState([]);
+
+    //Read all data from logged in company database.
+    getDocs(
+      query(collection(db, "Adverts"), where("Applicants", "==", username))
+      ).then((docSnap) => {
+        let jobs = [];
+        docSnap.forEach((doc) => {
+          jobs.push({ ...doc.data(), id: doc.id, title});
+        });
+        setUserJobs(jobs);
+        getData();
+      
+      });
+
+      const getData = async () => {
+        try {
+          const value = await AsyncStorage.getItem("Username");
+          var usernameFromAsyncStorage = value.toString();
+          if (value !== null) {
+            // value previously stored
+            setUsername(usernameFromAsyncStorage);
+          }
+        } catch (e) {
+          // error reading value
+        }
+      };
+      
+     
+
+
   return (
     <View style={styles.container}>
-      <Text>user view jobs Screen</Text>
+      <Text style={styles.company_username}>Job applications for {username}</Text>
+      
+      <View style={styles.mainContainer}>
+        <FlatList
+          data={userJobs}
+          renderItem={({ item }) => (
+            <View style={styles.innerContainer}>
+              <Text>{item.title}</Text>
+
+            </View>
+          )}
+        />
+      </View>
+
+      <View style={styles.navBar}>
+        <TouchableOpacity
+          style={styles.navButtons}
+          onPress={() => navigation.navigate("UserHomeScreen")}
+        >
+          <Image
+            style={{ width: 30, height: 30, margin: 15 }}
+            source={require("../assets/Home.png")}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.navButtons}
+          onPress={() => navigation.navigate("UserMessage")}
+        >
+          <Image
+            style={{ width: 25, height: 25, margin: 15 }}
+            source={require("../assets/Msg.png")}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.navButtons}
+          onPress={() => navigation.navigate("UserNotification")}
+        >
+          <Image
+            style={{ width: 25, height: 25, margin: 15 }}
+            source={require("../assets/Noti.png")}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.navButtons}
+          onPress={() => navigation.navigate("UserProfile")}
+        >
+          <Image
+            style={{ width: 25, height: 25, margin: 15 }}
+            source={require("../assets/Profile.png")}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
-
+          }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#F1F1F1",
     alignItems: "center",
     justifyContent: "center",
   },
-
+  mainContainer: {
+    flex: 2,
+    width: 350,
+    margin: 15,
+    shadowColor: "#000",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    borderRadius: 40,
+    borderWidth: 2,
+    borderColor: "navy",
+  },
   navBar: {
     flexDirection: "row",
+    flex: 4,
     backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
+    position: "absolute",
+    bottom: 0,
+    zIndex: 999,
+  },
+  navButtons: {
+    margin: 20,
+  },
+  userImg: {
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    marginBottom: 10,
+  },
+  company_username: {
+    textAlign: "center",
+    color: "navy",
+    fontSize: 25,
+    fontWeight: "bold",
+    letterSpacing: 0,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    marginTop: 40,
+  },
+  buttonTopNav: {
+    borderRadius: 10,
+    marginLeft: 5,
+    backgroundColor: "navy",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 30,
+    marginLeft: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+  },
+  buttons: {
+    flexDirection: "row",
+  },
+  buttonTopNavText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 14,
+    textAlign: "center",
+  },
+  company_info: {
+    color: "black",
+    textAlign: "center",
+    marginBottom: 10,
+    margin: 5,
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+  info_titles: {
+    fontSize: 20,
+    opacity: 0.5,
+    marginTop: 15,
+    marginLeft: 8,
+    textAlign: "center",
+  },
+  logOutButton: {
+    borderRadius: 10,
+    marginLeft: 5,
+    backgroundColor: "navy",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 30,
+    marginLeft: 10,
+    width: 100,
+    shadowColor: "#000",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
   },
 });
 
